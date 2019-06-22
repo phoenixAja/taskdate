a290041e605e23e99fba871c3ab2d81ed255e720
Lucy Li <mengqi.li09@imperial.ac.uk>
2019-06-22 15:30:50 -0700

    added taskdateapp

---
+++ taskdate/taskdateapp/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  taskdateapp
+//
+//  Created by Lucy Li on 22/06/2019.
+//  Copyright © 2019 PLLL. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ taskdate/taskdateappTests/taskdateappTests.swift
@@ -0,0 +1,34 @@
+//
+//  taskdateappTests.swift
+//  taskdateappTests
+//
+//  Created by Lucy Li on 22/06/2019.
+//  Copyright © 2019 PLLL. All rights reserved.
+//
+
+import XCTest
+@testable import taskdateapp
+
+class taskdateappTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
